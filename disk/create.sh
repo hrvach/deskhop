b4f561bd0a08b8cd05059990aaf0e093ec48a936
Igor <xdesigns@gmail.com>
2024-10-02 16:56:12 -0400

    Changes to build

--- disk/create.sh
+++ disk/create.sh
@@ -3,7 +3,7 @@
 
 dd if=/dev/zero of=fat.img bs=2M count=1
 
-mkdosfs  -F12 -n DESKHOP -i 0 fat.img
+mkdosfs  -F12 -n LOOPMOTION -i 0 fat.img
 
 sudo mount -o loop,x-mount.mkdir -t vfat fat.img /mnt/disk/
 sudo cp ../webconfig/config.htm /mnt/disk/config.htm
