51dec464318dafb96b16cb0fe0b22f9d15b7fd0e
Igor <xdesigns@gmail.com>
2024-12-14 14:49:07 -0500

    Interface changes

--- webconfig/form.py
+++ webconfig/form.py
@@ -9,7 +9,8 @@ class FormField:
     default: int | None = None
     values: dict[int, str] = field(default_factory=dict)
     data_type: str = "int32"
-    elem: str | None = None
+    elem: str | None = None,
+    style: str | None = None
 
 SHORTCUTS = {
     0x73: "None",
@@ -30,7 +31,7 @@ CONFIG_ = [
     FormField(1001, "Mouse", elem="label"),
     FormField(71, "Force Mouse Boot Mode", None, {}, "uint8", "checkbox"),
     FormField(75, "Enable Acceleration", None, {}, "uint8", "checkbox"),
-    FormField(77, "Jump Treshold", 0, {"min": 0, "max": 1024}, "uint16", "range"),
+    FormField(77, "Mouse transition force (Best set to 3) ", 3, {"min": 0, "max": 1024}, "uint16", "range"),
 
     FormField(1002, "Keyboard", elem="label"),
     FormField(72, "Force KBD Boot Protocol", None, {}, "uint8", "checkbox"),
@@ -41,18 +42,20 @@ CONFIG_ = [
 
 OUTPUT_ = [
     FormField(1, "Screen Count", 1, {1: "1", 2: "2", 3: "3"}, "uint32"),
-    FormField(2, "Speed X", 16, {"min": 1, "max": 100}, "int32", "range"),
-    FormField(3, "Speed Y", 16, {"min": 1, "max": 100}, "int32", "range"),
-    FormField(4, "Border Top", None, {}, "int32"),
-    FormField(5, "Border Bottom", None, {}, "int32"),
-    FormField(6, "Operating System", 1, {1: "Linux", 2: "MacOS", 3: "Windows", 4: "Android", 255: "Other"}, "uint8"),
     FormField(7, "Screen Position", 1, {1: "Left", 2: "Right"}, "uint8"),
-    FormField(8, "Cursor Park Position", 0, {0: "Top", 1: "Bottom", 3: "Previous"}, "uint8"),
-    FormField(1003, "Screensaver", elem="label"),
-    FormField(9, "Mode", 0, {0: "Disabled", 1: "Pong", 2: "Jitter"}, "uint8"),
-    FormField(10, "Only If Inactive", None, {}, "uint8", "checkbox"),
+    FormField(6, "Operating System", 1, {1: "Linux", 2: "MacOS", 3: "Windows", 4: "Android", 255: "Other"}, "uint8"),
+    FormField(1003, "Screensaver", elem="label", style="Title"),
+    FormField(9, "Type", 0, {0: "Disabled", 1: "Pong", 2: "Jitter"}, "uint8"),
+    # FormField(10, "Only If Inactive", None, {}, "uint8", "checkbox"),
     FormField(11, "Idle Time (μs)", None, {}, "uint64"),
     FormField(12, "Max Time (μs)", None, {}, "uint64"),
+    FormField(1004, "Mouse Acceleration", elem="label", style="Title"),
+    FormField(2, "Speed X", 16, {"min": 1, "max": 100}, "int32", "range"),
+    FormField(3, "Speed Y", 16, {"min": 1, "max": 100}, "int32", "range"),
+    # FormField(8, "Cursor Park Position", 0, {0: "Top", 1: "Bottom", 3: "Previous"}, "uint8"),
+    FormField(1005, "Precision Screen Alignment", elem="label", style="Title"),
+    FormField(4, "Border Top", None, {}, "int32"),
+    FormField(5, "Border Bottom", None, {}, "int32"),
 ]
 
 def generate_output(base, data):
@@ -64,6 +67,7 @@ def generate_output(base, data):
             "values": field.values,
             "type": field.data_type,
             "elem": field.elem,
+            "style": field.style,
         }
         for field in data
     ]
