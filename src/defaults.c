27c981ac2bc0ae11f10ada5d77fa86705abef492
Hrvoje Cavrak <hrvoje@hrvoje.org>
2025-02-09 16:25:38 +0100

    Various bugfixes and improvements   - Firmware version and checksum were incorrectly parsed.     Welp, this is embarassing - struct defined u32, but struct.pack in     Python helper packed it as 16-bit.            ->> Might require manual fw upgrade <<-   - Fixed threshold issue setting for jumping between outputs (#192)   - Fixed hotkey configuration clowntown (#197)

--- src/defaults.c
+++ src/defaults.c
@@ -58,7 +58,7 @@ const config_t default_config = {
     .force_kbd_boot_protocol = ENFORCE_KEYBOARD_BOOT_PROTOCOL,
     .force_mouse_boot_mode = false,
     .enable_acceleration = ENABLE_ACCELERATION,
-    .hotkey_toggle = HID_KEY_F24,
+    .hotkey_toggle = HOTKEY_TOGGLE,
     .kbd_led_as_indicator = KBD_LED_AS_INDICATOR,
-    .jump_threshold = 0,
+    .jump_threshold = JUMP_THRESHOLD,
 };
--- src/keyboard.c
+++ src/keyboard.c
@@ -17,7 +17,7 @@
 
 hotkey_combo_t hotkeys[] = {
     /* Main keyboard switching hotkey */
-    {.modifier       = KEYBOARD_MODIFIER_LEFTCTRL,
+    {.modifier       = HOTKEY_MODIFIER,
      .keys           = {HOTKEY_TOGGLE},
      .key_count      = 1,
      .pass_to_os     = false,
